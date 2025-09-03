dafbaf86657668ff1105932dd978c726e33e1223
Pedro <pedrosava01@gmail.com>
2025-09-03 15:49:06 -0600

    [Docker]:DockerFile y application.properties

--- MtdrSpring/setup.sh
+++ MtdrSpring/setup.sh
@@ -18,5 +18,5 @@ SETUP_SCRIPT="$MTDRWORKSHOP_LOCATION/utils/main-setup.sh"
 if ps -ef | grep "$SETUP_SCRIPT" | grep -v grep; then
   echo "The $SETUP_SCRIPT is already running.  If you want to restart it then kill it and then rerun."
 else
-  $SETUP_SCRIPT 2>&1 | tee -ai $MTDRWORKSHOP_LOG/main-setup.log
+ source "$SETUP_SCRIPT" 2>&1 | tee -ai $MTDRWORKSHOP_LOG/main-setup.log
 fi
