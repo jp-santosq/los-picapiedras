3c0851892205cc99f7f0477f9be2dd9369ee369e
alejandrateran <a01236688@tec.mx>
2025-09-29 23:50:54 -0600

    Update login page

--- MtdrSpring/backend/src/main/frontend/src/pages/Login.tsx
+++ MtdrSpring/backend/src/main/frontend/src/pages/Login.tsx
@@ -7,9 +7,11 @@ import {
   Box,
   TextField,
   Button,
+  Link
 } from "@mui/material";
-import { LockOutlined } from "@mui/icons-material";
 import { useAuth } from "../context/AuthContext.tsx";
+import oracleName from "../images/oracleName.png";
+import oracleLogo from "../images/oracleLogo.png";
 
 function Login() {
   const { login,user } = useAuth();
@@ -29,28 +31,83 @@ function Login() {
   };
 
   return (
-    <Container maxWidth="md">
-      <Paper elevation={10} sx={{ marginTop: 5, padding: 2 }}>
-        <Avatar
-          sx={{
-            margin: "auto",
-            bgcolor: "secondary.main",
-            textAlign: "center",
-            mb: 1,
-          }}
-        >
-          <LockOutlined />
-        </Avatar>
-        <Typography component="h1" variant="h5" sx={{ textAlign: "center" }}>
-          Sign In
-        </Typography>
-        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+    <Box sx={{ 
+      minWidth: '100dvh',
+      minHeight: '100dvh',
+      position: 'relative',
+      background: 'linear-gradient(to bottom, #312D2A 8%, #F5F4F2 8%)', 
+    }}>
+      <Box sx={{ 
+        display: 'flex', 
+        alignItems: 'center',
+      }}>
+        <img
+        src={oracleLogo} 
+                alt="Oracle Logo" 
+                style={{ 
+                  marginTop: '20px',
+                  marginLeft: '40px',
+                  width: '50px', 
+                  height: 'auto', 
+                  maxWidth:'100%' }} 
+        />
+        <Typography
+              variant="h5" 
+                sx={{ 
+                  mt:2.1,
+                  ml: 0.6,
+                  fontWeight: 'thin',
+                  fontSize: 20,
+                  color: '#FFFFFF',
+                }}> 
+                Organizer 
+          </Typography>
+        </Box>
+    <Container maxWidth="sm" sx={{display: "grid", placeItems: "center", minHeight:"90dvh"}}>
+      <Paper elevation={6} sx={{  }}>
+        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 , px: 4, py:4, minWidth: '60dvh'}}>
+          <Box sx={{ display: "flex", flexDirection: "row", alignItems: "left", mb: 3 }}>
+            <img 
+              src={oracleName} 
+              alt="Oracle Logo" 
+              style={{ 
+                width: '150px', 
+                height: 'auto', 
+                maxWidth:'100%' }} 
+              />
+            <Typography
+            variant="h5" 
+              sx={{ 
+                ml: 2,
+                fontWeight: 'light',
+                fontSize: 20,
+                alignSelf: 'center'
+              }}> 
+              Organizer 
+              </Typography>
+          </Box>
+          <Typography
+            variant="h5" 
+              sx={{ 
+                mb: 1,
+                fontWeight: 'light',
+                fontSize: 16,
+                alignSelf: 'center'
+              }}> 
+              Correo electrónico 
+          </Typography>
+
           <TextField
-            label="Email"
             fullWidth
             required
             autoFocus
-            sx={{ mb: 2 }}
+            sx = {{ '& .MuiInputBase-root': { height: '40px'},
+                  '& input:-webkit-autofill': {
+                  '-webkit-box-shadow': '0 0 0 100px #fff inset',
+                  '-webkit-text-fill-color': '#000',
+                  'box-shadow': '0 0 0 100px #fff inset',
+                } 
+              }}
             value={mail}
             onChange={(e) => {
               setMail(e.target.value);
@@ -58,33 +115,87 @@ function Login() {
             }}
           />
 
+          <Typography
+            variant="h5" 
+              sx={{ 
+                mt: 2,
+                mb: 1,
+                fontWeight: 'light',
+                fontSize: 16,
+                alignSelf: 'center'
+              }}> 
+              Contraseña 
+            </Typography>
+
           <TextField
-            label="Password"
             fullWidth
             required
             type="password"
+            sx = {{ '& .MuiInputBase-root': { height: '40px'},
+                  '& input:-webkit-autofill': {
+                  '-webkit-box-shadow': '0 0 0 100px #fff inset',
+                  '-webkit-text-fill-color': '#000',
+                  'box-shadow': '0 0 0 100px #fff inset',
+                } 
+              }}
             value={password}
             onChange={(e) => {
               setPassword(e.target.value);
               setError(false);
             }}
           />
+            {error && (
+              <Typography 
+                color="error"
+                sx={{
+                  mt: 1,
+                  fontSize: '0.875rem',
+                  display: 'flex',
+                  gap: 1,
+                  minHeight: '24px'
+                }}
+              >
+                Usuario o contraseña incorrectos
+              </Typography>
+            )}
 
-          {error && (
-            <Typography color="error">
-              Usuario o contraseña incorrectos
-            </Typography>
-          )}
-          {user && (
-            <Typography color="primary">Bienvenido {user.name} </Typography>
-          )}
+          <Link
+            component="button"
+            variant="body2"
+            onClick={() => {
+              alert("Funcionalidad no implementada");
+            }}
+            sx={{
+              mt: 1,
+              fontWeight: 'light',
+              fontSize: 16,
+              color: 'primary.main',
+              textDecoration: 'none',
+              '&:hover': {
+                textDecoration: 'underline'
+              }
+            }}>
+            ¿Ha olvidado la contraseña?
+          </Link>
 
-          <Button type="submit" variant="contained" fullWidth sx={{ mt: 1 }}>
-            Sign In
+          <Button 
+          type="submit" 
+          variant="contained" 
+          sx={{ 
+            mt: 2,
+            width: '60%',
+            textTransform: 'none',
+            fontSize: 16,
+            bgcolor: '#312D2A',
+            borderRadius: 0,
+            '&:hover': { bgcolor: '#242220ff'}
+             }}>
+            Conectar
           </Button>
         </Box>
       </Paper>
     </Container>
+    </Box>
   );
 }
 
