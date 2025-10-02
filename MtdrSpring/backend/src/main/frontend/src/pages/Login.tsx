42ebfbded33967c23700e05a441757fea287191e
alejandrateran <a01236688@tec.mx>
2025-10-02 12:19:18 -0600

    Arreglo de barra superior

--- MtdrSpring/backend/src/main/frontend/src/pages/Login.tsx
+++ MtdrSpring/backend/src/main/frontend/src/pages/Login.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import {
   Container,
   Paper,
-  Avatar,
   Typography,
   Box,
   TextField,
@@ -14,7 +13,7 @@ import oracleName from "../images/oracleName.png";
 import oracleLogo from "../images/oracleLogo.png";
 
 function Login() {
-  const { login,user } = useAuth();
+  const { login, user } = useAuth();
 
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
@@ -31,171 +30,170 @@ function Login() {
   };
 
   return (
-    <Box sx={{ 
-      minWidth: '100dvh',
-      minHeight: '100dvh',
-      position: 'relative',
-      background: 'linear-gradient(to bottom, #312D2A 8%, #F5F4F2 8%)', 
-    }}>
-      <Box sx={{ 
-        display: 'flex', 
-        alignItems: 'center',
-      }}>
-        <img
-        src={oracleLogo} 
+    <>
+      <Paper sx={{ 
+          display: 'flex', 
+          alignItems: 'center',
+          minHeight: '8dvh',
+          backgroundColor: '#312D2A',
+          borderRadius: 0
+        }}>
+          <img
+          src={oracleLogo} 
+                  alt="Oracle Logo" 
+                  style={{ 
+                    width: '57px', 
+                    height: 'auto', 
+                    maxWidth:'100%',
+                    marginLeft: '25px' }} 
+          />
+          <Typography
+                variant="h5" 
+                  sx={{ 
+                    ml: 2,
+                    fontFamily: 'Oracle Sans',
+                    fontWeight: 'normal', 
+                    fontSize: 20,
+                    color: '#FFFFFF'
+                  }}> 
+                  Organizer 
+            </Typography>
+          </Paper>
+      <Container maxWidth="sm" sx={{display: "grid", placeItems: "center", minHeight:"90dvh"}}>
+        <Paper elevation={6} sx={{ alignItems: "center", display: "flex"}}>
+          <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 , px: 4, py:4, minWidth: '60dvh'}}>
+            <Box sx={{ display: "flex", flexDirection: "row", alignItems: "left", mb: 3 }}>
+              <img 
+                src={oracleName} 
                 alt="Oracle Logo" 
                 style={{ 
-                  marginTop: '20px',
-                  marginLeft: '40px',
-                  width: '50px', 
-                  height: 'auto', 
-                  maxWidth:'100%' }} 
-        />
-        <Typography
+                  width: 'auto', 
+                  height: '25px', 
+                  maxWidth:'100%',
+                  marginTop: 5 }} 
+                />
+              <Typography
               variant="h5" 
                 sx={{ 
-                  mt:2.1,
-                  ml: 0.6,
-                  fontWeight: 'thin',
-                  fontSize: 20,
-                  color: '#FFFFFF',
+                  ml: 1,
+                  fontFamily: 'Oracle Sans',
+                  fontWeight: 'normal',
+                  fontSize: 30,
+                  alignSelf: 'center'
                 }}> 
                 Organizer 
-          </Typography>
-        </Box>
-    <Container maxWidth="sm" sx={{display: "grid", placeItems: "center", minHeight:"90dvh"}}>
-      <Paper elevation={6} sx={{  }}>
-        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 , px: 4, py:4, minWidth: '60dvh'}}>
-          <Box sx={{ display: "flex", flexDirection: "row", alignItems: "left", mb: 3 }}>
-            <img 
-              src={oracleName} 
-              alt="Oracle Logo" 
-              style={{ 
-                width: '150px', 
-                height: 'auto', 
-                maxWidth:'100%' }} 
-              />
+                </Typography>
+            </Box>
             <Typography
-            variant="h5" 
-              sx={{ 
-                ml: 2,
-                fontWeight: 'light',
-                fontSize: 20,
-                alignSelf: 'center'
-              }}> 
-              Organizer 
-              </Typography>
-          </Box>
-          <Typography
-            variant="h5" 
-              sx={{ 
-                mb: 1,
-                fontWeight: 'light',
-                fontSize: 16,
-                alignSelf: 'center'
-              }}> 
-              Correo electrónico 
-          </Typography>
-
-          <TextField
-            fullWidth
-            required
-            autoFocus
-            sx = {{ '& .MuiInputBase-root': { height: '40px'},
-                  '& input:-webkit-autofill': {
-                  '-webkit-box-shadow': '0 0 0 100px #fff inset',
-                  '-webkit-text-fill-color': '#000',
-                  'box-shadow': '0 0 0 100px #fff inset',
-                } 
-              }}
-            value={mail}
-            onChange={(e) => {
-              setMail(e.target.value);
-              setError(false);
-            }}
-          />
-
-          <Typography
-            variant="h5" 
-              sx={{ 
-                mt: 2,
-                mb: 1,
-                fontWeight: 'light',
-                fontSize: 16,
-                alignSelf: 'center'
-              }}> 
-              Contraseña 
+              variant="h5" 
+                sx={{ 
+                  mb: 1,
+                  fontWeight: 'light',
+                  fontSize: 16,
+                  alignSelf: 'center'
+                }}> 
+                Correo electrónico 
             </Typography>
 
-          <TextField
-            fullWidth
-            required
-            type="password"
-            sx = {{ '& .MuiInputBase-root': { height: '40px'},
-                  '& input:-webkit-autofill': {
-                  '-webkit-box-shadow': '0 0 0 100px #fff inset',
-                  '-webkit-text-fill-color': '#000',
-                  'box-shadow': '0 0 0 100px #fff inset',
-                } 
-              }}
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-              setError(false);
-            }}
-          />
-            {error && (
-              <Typography 
-                color="error"
-                sx={{
-                  mt: 1,
-                  fontSize: '0.875rem',
-                  display: 'flex',
-                  gap: 1,
-                  minHeight: '24px'
+            <TextField
+              fullWidth
+              required
+              autoFocus
+              sx = {{ '& .MuiInputBase-root': { height: '40px'},
+                    '& input:-webkit-autofill': {
+                    '-webkit-box-shadow': '0 0 0 100px #fff inset',
+                    '-webkit-text-fill-color': '#000',
+                    'box-shadow': '0 0 0 100px #fff inset',
+                  } 
                 }}
-              >
-                Usuario o contraseña incorrectos
+              value={mail}
+              onChange={(e) => {
+                setMail(e.target.value);
+                setError(false);
+              }}
+            />
+
+            <Typography
+              variant="h5" 
+                sx={{ 
+                  mt: 2,
+                  mb: 1,
+                  fontWeight: 'light',
+                  fontSize: 16,
+                  alignSelf: 'center'
+                }}> 
+                Contraseña 
               </Typography>
-            )}
 
-          <Link
-            component="button"
-            variant="body2"
-            onClick={() => {
-              alert("Funcionalidad no implementada");
-            }}
-            sx={{
-              mt: 1,
-              fontWeight: 'light',
+            <TextField
+              fullWidth
+              required
+              type="password"
+              sx = {{ '& .MuiInputBase-root': { height: '40px'},
+                    '& input:-webkit-autofill': {
+                    '-webkit-box-shadow': '0 0 0 100px #fff inset',
+                    '-webkit-text-fill-color': '#000',
+                    'box-shadow': '0 0 0 100px #fff inset',
+                  } 
+                }}
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setError(false);
+              }}
+            />
+              {error && (
+                <Typography 
+                  color="error"
+                  sx={{
+                    mt: 1,
+                    fontSize: '0.875rem',
+                    display: 'flex',
+                    gap: 1,
+                    minHeight: '24px'
+                  }}
+                >
+                  Usuario o contraseña incorrectos
+                </Typography>
+              )}
+
+            <Link
+              component="button"
+              variant="body2"
+              onClick={() => {
+                alert("Funcionalidad no implementada");
+              }}
+              sx={{
+                mt: 1,
+                fontWeight: 'normal',
+                fontSize: 16,
+                color: 'primary.main',
+                textDecoration: 'none',
+                '&:hover': {
+                  textDecoration: 'underline'
+                }
+              }}>
+              ¿Ha olvidado la contraseña?
+            </Link>
+            <br></br>
+            <Button 
+            type="submit" 
+            variant="contained" 
+            sx={{ 
+              mt: 2,
+              width: '50%',
+              textTransform: 'none',
               fontSize: 16,
-              color: 'primary.main',
-              textDecoration: 'none',
-              '&:hover': {
-                textDecoration: 'underline'
-              }
-            }}>
-            ¿Ha olvidado la contraseña?
-          </Link>
-
-          <Button 
-          type="submit" 
-          variant="contained" 
-          sx={{ 
-            mt: 2,
-            width: '60%',
-            textTransform: 'none',
-            fontSize: 16,
-            bgcolor: '#312D2A',
-            borderRadius: 0,
-            '&:hover': { bgcolor: '#242220ff'}
-             }}>
-            Conectar
-          </Button>
-        </Box>
-      </Paper>
-    </Container>
-    </Box>
+              bgcolor: '#312D2A',
+              borderRadius: 0,
+              '&:hover': { bgcolor: '#242220ff'}
+               }}>
+              Conectar
+            </Button>
+          </Box>
+        </Paper>
+      </Container>
+    </>
   );
 }
 
